2911add3b3516b74686c5dd685215c3a33468b02
haven75 <wenquantongji@163.com>
2015-10-28 20:20:14 +0800

    add an ms delay function

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include"stdio.h"
 
 
-void delay(us);
+void delay(int us);
+void delay(int ms);
 void main()
 {
 	cout<<"hello world!"<<endl;
 	delay();
 }
 
-void delay(us)
+void delay(int us)
 {
 	unsigned int i,j=0;
 	for(i=0;i<us;i++)
+	for(j=100;j>0;j++);
+}
+
+
+void delay(int ms)
+{
+	unsigned int i,j=0;
+	for(i=0;i<ms;i++)
 	for(j=100;j>0;j++);
 }
